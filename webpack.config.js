08739bb6fa0235b22b9cada16c13aaa12dd3d98b
FannyVA <fannyviveros82@gmail.com>
2023-05-13 04:43:12 -0600

    Proyecto

--- webpack.config.js
+++ webpack.config.js
@@ -15,17 +15,21 @@ module.exports = {
   ],
   module: {
     rules: [
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
       {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
       },
-    ],
+    ]
   },
   devServer: {
-    static: {
-      directory: path.join(__dirname, 'public'),
-    },
-    port: 3000,
-    hot: true,
+    // contentBase: path.resolve(__dirname, 'dist'),
+    static: path.resolve(__dirname, 'dist')
   }
 }
