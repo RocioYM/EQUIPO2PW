e34d8bb4830a58f0590ee8ec73120288f4098cbd
RocioYM <desilverrocio@yahoo.com>
2023-06-19 09:42:46 -0600

    menu ambu

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,29 @@
-import css from "../css/styles.css";
-import "bootstrap";
-import "../css/bootstrap.min.css";
+//import css from "../css/styles.css";
+// import "bootstrap";
+// import "../css/bootstrap.min.css";
 
-const get_meal_btn = document.getElementById("get_meal");
 
-get_meal_btn.addEventListener("click",() =>{
-    fetch('https://www.themealdb.com/api/json/v1/1/random.php')
-    .then(res => res.json())
-    .then(res => {
-        createMeal(res.meals[0]);
-    })
-    .catch(e => {
-        console.warn(e);
+setTimeout(() => {
+    const get_meal_btn = document.getElementById("btn-aleatorio");
+    const container_meals_div = document.getElementById("container_meals");
+    const nombre_comida_label = document.getElementById("nombre_comida");
+    const imagen = document.getElementById("imagenComida");
+
+    get_meal_btn.addEventListener("click",() =>{
+        fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+        .then(res => res.json())
+        .then(res => {
+            createMeal(res.meals[0]);
+        })
+        .catch(e => {
+            console.warn(e);
+        });
     });
-});
+    
+    function createMeal(meal){
+        nombre_comida_label.innerHTML = meal.strMeal;
+        //categoria_comida_label.innerHTML = meal.strCategory;
+        imagen.src = (meal.strMealThumb);
+    }
 
+  }, "1000");
